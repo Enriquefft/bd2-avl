040a98c4894cbaf097c3e3e82657dafae03edbca
Enrique <enrique.flores@utec.edu.pe>
2023-08-24 12:22:12 -0500

    avl imp and minor fixes on avl file

--- avlfile.cpp
+++ avlfile.cpp
@@ -1,70 +1,70 @@
+#include <cstdint>
+#include <string>
+#include <utility>
 #include <vector>
+
 using namespace std;
 
-struct Record
-{
-    int cod;
-	char nombre[12];
-	int ciclo;
-	
-	long left, right;
+constexpr size_t MAX_NAME_LENGTH = 12;
+
+#pragma pack(push, 1)
+struct Record {
+  int cod;
+  char nombre[MAX_NAME_LENGTH];
+  int ciclo;
+
+  int64_t left;
+  int64_t right;
 };
+#pragma pack(pop)
 
-class AVLFile
-{
+class AVLFile {
 private:
-    string filename;
-    long pos_root;
+  std::string m_filename;
+  int64_t m_pos_root;
+
 public:
-    AVLFile(string filename){
-		this pos_root = 0;
-		this->filename = filename;
-	}
-	
-    Record find(int key){
-		return find(pos_root, key);
-	}
-	
-	void insert(Record record){
-		insert(po_root, record);
-	}
-	
-    vector<Record> inorder(){
-		return inorder(pos_root);
-	}
+  explicit AVLFile(string filename)
+      : m_filename(std::move(filename)), m_pos_root(0) {}
+
+  Record find(int key) { return find(m_pos_root, key); }
+
+  void insert(Record record) { insert(m_pos_root, record); }
+
+  vector<Record> inorder() { return inorder(m_pos_root); }
 
 private:
-	Record find(long pos_node, int key){
-		/*
-		if (node == nullptr)
-			return false;
-		else if (value < node->data)
-			return find(node->left, value);
-		else if (value > node->data)
-			return find(node->right, value);
-		else
-			return true;
-		*/
-	}	
+  Record find(long pos_node, int key) {
+    /*
+    if (node == nullptr)
+            return false;
+    else if (value < node->data)
+            return find(node->left, value);
+    else if (value > node->data)
+            return find(node->right, value);
+    else
+            return true;
+    */
+  }
 
-	void insert(long pos_node, Record record){
-		/*
-		if (node == nullptr)
-			node = new NodeBT<T>(value);
-		else if (value < node->data)
-			insert(node->left, value);
-		else
-			insert(node->right, value); 
-		*/
-	}
-	
-	vector<Record> inorder(long pos_node){
-		/*
-		if (node == nullptr)
-			return;
-		displayPreOrder(node->left);
-		cout << node->data << endl;
-		displayPreOrder(node->right);
-		*/
-	}
+  void insert(long pos_node, Record record) {
+    /*
+    if (node == nullptr)
+            node = new NodeBT<T>(value);
+    else if (value < node->data)
+            insert(node->left, value);
+    else
+            insert(node->right, value);
+    */
+  }
+
+  vector<Record> inorder(long pos_node) {
+    /*
+    if (node == nullptr)
+            return;
+    displayPreOrder(node->left);
+    cout << node->data << endl;
+    displayPreOrder(node->right);
+    */
+  }
 };
