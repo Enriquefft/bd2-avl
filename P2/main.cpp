a83875d62daa19138ad035860e6dd30897dbebe4
Enrique <enrique.flores@utec.edu.pe>
2023-08-26 08:16:10 -0500

    Moved test to main

--- P2/main.cpp
+++ P2/main.cpp
@@ -1,3 +1,31 @@
-#include "RandomFile.hpp"
+#include "RandomIndex.hpp"
+#include <iostream>
 
-int main(int argc, char *argv[]) { return 0; }
+namespace {
+[[maybe_unused]] void write() {
+
+  RandomIndex idx;
+  // Record record(1, "n", 1);
+  Record record(2, "n", 1);
+  Record record2(3, "n", 1);
+  idx.insert(record);
+  idx.insert(record2);
+}
+
+[[maybe_unused]] void read() {
+
+  RandomIndex idx;
+  for (const auto &[key, record] : idx) {
+    std::cout << key << " " << record << std::endl;
+  }
+}
+
+} // namespace
+
+int main() {
+
+  write();
+  read();
+
+  return 0;
+}
