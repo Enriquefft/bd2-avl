c6d83831759493b4bc84772f38651222aa5b3bd9
Enrique <enrique.flores@utec.edu.pe>
2023-08-25 23:46:56 -0500

    Inherited index from std::unordered_map

--- P2/RandomIndex.hpp
+++ P2/RandomIndex.hpp
@@ -2,16 +2,27 @@
 #define RANDOM_INDEX_HPP
 
 #include "Record.hpp"
+#include <iostream>
 #include <string>
 #include <unordered_map>
 
 constexpr std::string_view DEFAULT_INDEX_FILE = "index.dat";
 
-class RandomIndex : std::unordered_map<Record::key_type, Record> {
+class RandomIndex : public std::unordered_map<Record::key_type, Record> {
+  using base_class = std::unordered_map<Record::key_type, Record>;
+
 public:
-  explicit RandomIndex(
-      std::string_view index_file = DEFAULT_INDEX_FILE); // Read index
-  ~RandomIndex();                                        // write index
+  // Inherit constructors
+  using base_class::unordered_map;
+  using base_class::operator=;
+
+  explicit RandomIndex(std::string_view index_file = DEFAULT_INDEX_FILE)
+      : m_index_file(index_file) {
+    std::cout << "Loading index from " << index_file << std::endl;
+  }
+  ~RandomIndex() {
+    std::cout << "Saving index to " << m_index_file << std::endl;
+  }
 
 private:
   std::string m_index_file;
